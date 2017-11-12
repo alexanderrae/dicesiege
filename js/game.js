b57713461a5e8e4a079451794bb544606f4e47d4
alexanderrae <31792261+alexanderrae@users.noreply.github.com>
2017-11-12 20:17:36 +0100

    Update game.js

--- js/game.js
+++ js/game.js
@@ -596,13 +596,8 @@ var GameController = function (GameData) {
 
             for (var i = 0; i < attackP.troops; i++) attackPWR += getRandomInt(1,7);
             for (var i = 0; i < defendP.troops; i++) defendPWR += getRandomInt(1,7);
-            
-         //   var random1 = Math.floor(Math.random());
-         //   var random2 = Math.floor(Math.random());
-            
+     
             var success = attackPWR > defendPWR;
-            
-         //   success = 1;
 
             console.log("Attack: " + attackPWR);
             console.log("Defend: " + defendPWR);
