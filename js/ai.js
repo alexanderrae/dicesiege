6f531b6e1eafe81a08c3da3c9f508d44c95412bd
alexanderrae <31792261+alexanderrae@users.noreply.github.com>
2017-11-15 15:08:51 +0100

    Update ai.js

--- js/ai.js
+++ js/ai.js
@@ -2,12 +2,15 @@ while (true) {
   if (Game.Data.current_player != 1) {
     var current_player = Game.Data.current_player;
     var ownedPIDS = Game.Data.player_info[current_player].ownedPIDS;
+    var n_players = Game.Data.n_players;
+    
     for (i = 0; i < ownedPIDS; i++) {
-      Game.Input.province.clicked(i);
-      var willAttack = Math.floor(Math.random);
-      console.log(willAttack);
-      sleep(10)
-      Game.Input.province.clicked(i);
+      Game.Input.province.clicked(ownedPIDS[i]);
+      var willAttack = (Math.random>0.5)?willAttack:true;
+      var j = Game.Data.player_info[1].ownedPIDS;
+      for (k = 0 ; k < j; k++) {
+      Game.Input.province.clicked(j[k]);
+      }
     }
   }
  
