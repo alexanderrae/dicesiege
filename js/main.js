750e4b696f89f4e63ffcfc463f820bff03f4ad04
alexanderrae <31792261+alexanderrae@users.noreply.github.com>
2017-11-15 15:58:31 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,6 +18,10 @@ function init() {
     // Start the game
     Game = new GameController(GameData);
     
+    //Start Artificial Intelligence
+  
+    startAI(Game);
+    
     // TableRender();
     Render = new Renderer();
     Render.init.all();
@@ -35,27 +39,7 @@ function reinit() {
     Game = new GameController(GameData);
     
     //Start Artificial Intelligence
-    
-    function startAI(Game) {
-    console.log("started ai");
-  while (Game.Data.current_player != 1) {
-    console.log("got past thisGame.Data..");
-    var current_player = Game.Data.current_player;
-    var ownedPIDS = Game.Data.player_info[current_player].ownedPIDS;
-    var n_players = Game.Data.n_players;
-    
-    for (i = 0; i < ownedPIDS; i++) {
-      Game.Input.province.clicked(ownedPIDS[i]);
-      var willAttack = (Math.random>0.5)?willAttack:true;
-      var j = Game.Data.player_info[1].ownedPIDS;
-      for (k = 0 ; k < j; k++) {
-      Game.Input.province.clicked(j[k]);
-      }
-    }
-  }
-}
-
-
+  
     startAI(Game);
 
 
