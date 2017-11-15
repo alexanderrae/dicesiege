674bccf7cfcb3b0cab7c7bfce7251eefa1eef8df
alexanderrae <31792261+alexanderrae@users.noreply.github.com>
2017-11-15 11:56:32 +0100

    Update ai.js

--- js/ai.js
+++ js/ai.js
@@ -1,2 +1,12 @@
 while (true) {
+  if (Game.Data.current_player != 1) {
+    var current_player = Game.Data.current_player;
+    var ownedPIDS = Game.Data.player_info[current_player].ownedPIDS;
+    for (i = 0; i < ownedPIDS; i++) {
+      Game.Input.province.clicked(i);
+      sleep(10)
+      Game.Input.province.clicked(i);
+    }
+  }
+ 
 }
