294ee88229b41fc10e60009a20d2ef17deb52c0c
alexanderrae <31792261+alexanderrae@users.noreply.github.com>
2017-11-15 14:53:10 +0100

    Update ai.js

--- js/ai.js
+++ js/ai.js
@@ -4,6 +4,8 @@ while (true) {
     var ownedPIDS = Game.Data.player_info[current_player].ownedPIDS;
     for (i = 0; i < ownedPIDS; i++) {
       Game.Input.province.clicked(i);
+      var willAttack = Math.floor(Math.random);
+      console.log(willAttack);
       sleep(10)
       Game.Input.province.clicked(i);
     }
