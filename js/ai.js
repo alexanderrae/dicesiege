840e9681e88994dc2a4b4a4f901f9744b45da508
alexanderrae <31792261+alexanderrae@users.noreply.github.com>
2017-11-17 10:48:21 +0100

    Update ai.js

--- js/ai.js
+++ js/ai.js
@@ -1,35 +1,15 @@
 function startAI(GameData) {
     console.log("Hello Dave");
     var Game = GameData;
-    console.log("Number of players: " + Game.Data.n_players);
     
     var current_player = Game.Data.current_player;
-    console.log(current_player);
     var ownedPIDs = Game.Data.player_info[current_player].ownedPIDs;
-    console.log(ownedPIDs.length);
     var n_players = Game.Data.n_players;
-    console.log(n_players);
-    Game.Input.province.clicked(ownedPIDs[1]);
-    console.log("Clicked " + ownedPIDs[1]);
     
-    
-    var bordering = Game.Data.provinces[1].bordering;
- 
-    for ( j = 0 ; j < bordering.length ; j++) {
+    for ( i = 0 ; i < ownedPIDs.length ; i++) {
         
-    Game.Input.province.clicked(bordering[j]);
+    Game.Input.province.clicked(ownedPIDs[i]);    
+    var bordering = Game.Data.provinces[1].bordering;    
+    Game.Input.province.clicked(bordering[Math.floor(Math.random()*(bordering.length+1))]);
     }
-
-    
-   /* 
-    for (i = 0; i < ownedPIDs.length; i++) {
-      Game.Input.province.clicked(ownedPIDs[i]);
-        console.log("clicked " + ownedPIDs[i];
-      var willAttack = (Math.random>0.5)?willAttack:true;
-      var j = Game.Data.player_info[1].ownedPIDS;
-      for (k = 0 ; k < j; k++) {
-      Game.Input.province.clicked(j[k]);
-      }
-    }
-    */
   }
